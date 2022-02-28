e41595c86112bfc2ba41a565bf0320d6c7527243
moeinnoghani <mo.noghani@greenweb.local>
2022-02-28 09:10:26 +0330

    add a rout for email

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::any('/email', [\App\Http\Controllers\EmailController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('/email',function (Request $request){
 
-}
-);
